f6bd170e25649639b1f815a924069832bec9549d
Daniel Victor <ilauncherdeveloper@gmail.com>
2026-03-19 20:21:34 -0300

    Improve byteex.h

--- boot/freeldr/freeldr/include/bytesex.h
+++ boot/freeldr/freeldr/include/bytesex.h
@@ -1,11 +1,40 @@
 #pragma once
 
+static inline VOID __SWAPB(PUCHAR B, ULONG S)
+{
+    ULONG HalfS = S >> 1;
+
+    for (ULONG i = 0, j = S - 1; i < HalfS; i++, j--)
+    {
+        UCHAR B1 = B[i];
+        UCHAR B2 = B[j];
+
+        B[i] = B2;
+        B[j] = B1;
+    }
+}
+
+#define __SWAPX(Name, Type)                         \
+    static inline Type Name(Type Value)             \
+    {                                               \
+        Type Swapped = Value;                       \
+        __SWAPB((PUCHAR)&Swapped, sizeof(Swapped)); \
+        return Swapped;                             \
+    }
+
+__SWAPX(__SWAPW, USHORT)
+__SWAPX(__SWAPD, ULONG)
+__SWAPX(__SWAPQ, ULONGLONG)
+
 #ifdef _PPC_
-#define SWAPD(x) ((((x)&0xff)<<24)|(((x)&0xff00)<<8)|(((x)>>8)&0xff00)|(((x)>>24)&0xff))
-#define SWAPW(x) ((((x)&0xff)<<8)|(((x)>>8)&0xff))
+#define SWAPQ(x) __SWAPQ(x)
+#define SWAPD(x) __SWAPD(x)
+#define SWAPW(x) __SWAPW(x)
 #else
+#define SWAPQ(x) x
 #define SWAPD(x) x
 #define SWAPW(x) x
 #endif
+#define SQ(Object,Field) Object->Field = SWAPQ(Object->Field)
 #define SD(Object,Field) Object->Field = SWAPD(Object->Field)
 #define SW(Object,Field) Object->Field = SWAPW(Object->Field)
